4897996b7b73f802b325dac1d57bbe40b2c1d82c
WilkinsonMaciel <wms5@cin.ufpe.br>
2018-12-05 21:13:34 -0200

    aplicativo teste

---
+++ import/app/src/main/java/com/example/wilkinsonmaciel/animport/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.wilkinsonmaciel.animport
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        butao.setOnClickListener{
+            startActivity( Intent( this ,Main2Activity::class.java) )
+        }
+    }
+}
