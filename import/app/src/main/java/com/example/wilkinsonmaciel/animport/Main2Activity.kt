b7df43e3bd5e6f9bbe6cfb6df5aab172b7d3e85e
WilkinsonMaciel <wms5@cin.ufpe.br>
2018-12-05 23:52:17 -0200

    import atualizado

--- import/app/src/main/java/com/example/wilkinsonmaciel/animport/Main2Activity.kt
+++ import/app/src/main/java/com/example/wilkinsonmaciel/animport/Main2Activity.kt
@@ -18,10 +18,10 @@ class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         setContentView(R.layout.activity_main2)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
+        /*fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
-        }
+        }*/
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
